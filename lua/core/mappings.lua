71bb1bbd235f2e501d7e50a4990ef1632f708d34
tippy <tippy@mail.com>
2024-09-07 12:50:14 +0300

    update

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -12,4 +12,8 @@ vim.keymap.set('n', '<leader>X', ':BufferLineCloseOthers<CR>')
 vim.keymap.set('n', '<leader>b', ':BufferLinePick<CR>')
 
 
-
+-- switch work panel
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true})
